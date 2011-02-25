c73bba03f8c7acd02e27590439c0aceae9fe496d
Harald Hoyer <harald@redhat.com>
2011-02-25 12:06:15 +0100

    parse-biosdevname: put parenthesis around BIOSDEVNAME env var

--- modules.d/97biosdevname/parse-biosdevname.sh
+++ modules.d/97biosdevname/parse-biosdevname.sh
@@ -1,7 +1,7 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 USE_BIOSDEVNAME=$(getarg biosdevname)
-if [ $USE_BIOSDEVNAME != "1" ]; then
+if [ "$USE_BIOSDEVNAME" != "1" ]; then
     udevproperty UDEV_BIOSDEVNAME=
     rm -f /etc/udev/rules.d/71-biosdevname.rules
 else
