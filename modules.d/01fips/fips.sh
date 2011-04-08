592dabba90903b66c13f515d849782037cd8bfb4
Harald Hoyer <harald@redhat.com>
2011-04-08 12:14:58 +0200

    fips: moved to pre-pivot to support /boot in /

--- modules.d/01fips/fips.sh
+++ modules.d/01fips/fips.sh
@@ -1,60 +1,65 @@
 #!/bin/sh
-
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
 do_fipskernel()
 {
     boot=$(getarg boot=)
-    KERNEL=$(uname -r)
-    case "$boot" in
-    LABEL=*)
-        boot="$(echo $boot | sed 's,/,\\x2f,g')"
-        boot="/dev/disk/by-label/${boot#LABEL=}"
-        ;;
-    UUID=*)
-        boot="/dev/disk/by-uuid/${boot#UUID=}"
-        ;;
-    /dev/*)
-        ;;
-    *)
-        die "You have to specify boot=<boot device> as a boot option for fips=1" ;;
-    esac
+    newroot=$NEWROOT
 
-    if ! [ -e "$boot" ]; then
-        udevadm trigger --action=add >/dev/null 2>&1
-        [ -z "$UDEVVERSION" ] && UDEVVERSION=$(udevadm --version)
+    if [ -n "$boot" ]; then
+        KERNEL=$(uname -r)
+        case "$boot" in
+        LABEL=*)
+            boot="$(echo $boot | sed 's,/,\\x2f,g')"
+            boot="/dev/disk/by-label/${boot#LABEL=}"
+            ;;
+        UUID=*)
+            boot="/dev/disk/by-uuid/${boot#UUID=}"
+            ;;
+        /dev/*)
+            ;;
+        *)
+            die "You have to specify boot=<boot device> as a boot option for fips=1" ;;
+        esac
 
-        if [ $UDEVVERSION -ge 143 ]; then
-            udevadm settle --exit-if-exists=$boot
-        else
-            udevadm settle --timeout=30
+        if ! [ -e "$boot" ]; then
+            udevadm trigger --action=add >/dev/null 2>&1
+            [ -z "$UDEVVERSION" ] && UDEVVERSION=$(udevadm --version)
+            
+            if [ $UDEVVERSION -ge 143 ]; then
+                udevadm settle --exit-if-exists=$boot
+            else
+                udevadm settle --timeout=30
+            fi
         fi
+
+        [ -e "$boot" ]
+        
+        mkdir /boot
+        info "Mounting $boot as /boot"
+        mount -oro "$boot" /boot
+        unset newroot
     fi
-
-    [ -e "$boot" ]
-
-    mkdir /boot
-    info "Mounting $boot as /boot"
-    mount -oro "$boot" /boot
 
     info "Checking integrity of kernel"
 
-    if ! [ -e "/boot/.vmlinuz-${KERNEL}.hmac" ]; then
-        warn "/boot/.vmlinuz-${KERNEL}.hmac does not exist"
+    if ! [ -e "$newroot/boot/.vmlinuz-${KERNEL}.hmac" ]; then
+        warn "$newroot/boot/.vmlinuz-${KERNEL}.hmac does not exist"
         return 1
     fi
 
-    sha512hmac -c "/boot/.vmlinuz-${KERNEL}.hmac" || return 1
+    sha512hmac -c "$newroot/boot/.vmlinuz-${KERNEL}.hmac" || return 1
 
-    info "Umounting /boot"
-    umount /boot
+    if [ -z "$newroot" ]; then
+        info "Umounting /boot"
+        umount /boot
+    fi
 }
 
 do_fips()
 {
     FIPSMODULES=$(cat /etc/fipsmodules)
 
-    if ! getarg rd.fips.skipkernel >/dev/null; then
-	do_fipskernel
-    fi
     info "Loading and integrity checking all crypto modules"
     for module in $FIPSMODULES; do
         if [ "$module" != "tcrypt" ]; then
@@ -76,5 +81,3 @@ else
     do_fips || die "FIPS integrity test failed"
     set +e
 fi
-
-# vim:ts=8:sw=4:sts=4:et
