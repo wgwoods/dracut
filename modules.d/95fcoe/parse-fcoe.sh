c61cec19fa5a6f602d1942d60cdc85b839691a29
Harald Hoyer <harald@redhat.com>
2011-04-08 10:21:14 +0200

    fcoe/parse-fcoe.sh: s#source#.#g
    dash does not understand "source"

--- modules.d/95fcoe/parse-fcoe.sh
+++ modules.d/95fcoe/parse-fcoe.sh
@@ -59,7 +59,7 @@ if [ "$fcoe_interface" = "edd" ]; then
                 unset fcoe_mac
                 unset fcoe_interface
                 fcoe_mac=$(cat ${nic}/address)
-                [ -n "$fcoe_mac" ] && source /sbin/fcoe-genrules.sh
+                [ -n "$fcoe_mac" ] && . /sbin/fcoe-genrules.sh
             fi
         done
     done
@@ -71,7 +71,7 @@ else
         if [ "$fcoe_dcb" != "nodcb" -a "$fcoe_dcb" != "dcb" ] ; then
             warn "Invalid FCoE DCB option: $fcoe_dcb"
         fi
-        source /sbin/fcoe-genrules.sh
+        . /sbin/fcoe-genrules.sh
     done
 fi
 
