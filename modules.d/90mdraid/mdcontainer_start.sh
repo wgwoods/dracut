655d9cb4a35095f9f2831e5efb9b315342694270
Harald Hoyer <harald@redhat.com>
2012-02-14 14:07:30 +0100

    90mdraid/mdcontainer_start.sh: do not start with "-I"

--- modules.d/90mdraid/mdcontainer_start.sh
+++ modules.d/90mdraid/mdcontainer_start.sh
@@ -4,7 +4,7 @@
 md=$1
 udevadm control --stop-exec-queue
 # and activate any containers
-mdadm -IR $md 2>&1 | vinfo
+mdadm -R $md 2>&1 | vinfo
 ln -s /sbin/mdraid-cleanup /pre-pivot/30-mdraid-cleanup.sh 2>/dev/null
 ln -s /sbin/mdraid-cleanup /pre-pivot/31-mdraid-cleanup.sh 2>/dev/null
 udevadm control --start-exec-queue
